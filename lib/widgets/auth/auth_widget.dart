d52b3d5ded0e6b4868e88ff8ad9332d0a0cefaed
Караул 1 <zavorgan@gmail.com>
2022-03-17 00:33:17 +0900

    16/03/2022

---
+++ lib/widgets/auth/auth_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AuthWidget extends StatefulWidget {
+  const AuthWidget({Key? key}) : super(key: key);
+
+  @override
+  _AuthWidgetState createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login to your account'),
+      ),
+      body: ListView(
+        children: [_HeaderWidget()],
+      ),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 25),
+          Text(
+              'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. Click here  to get started.'),
+          SizedBox(height: 25),
+          Text(
+              'If you signed up but didn`t get your verification email, click here to have it resent.'),
+        ],
+      ),
+    );
+  }
+}
