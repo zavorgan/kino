e0b7bf536ea5e022be76b96bd8d775bc99befb27
zavorgan <zavorgan@gmail.com>
2022-03-18 00:42:58 +0900

    18/03/2022_00:42

---
+++ lib/Theme/app_button_style.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// abstract class AppButtonStyle {
+//   static final ButtonStyle linkButton = ButtonStyle(
+//     foregroundColor: MaterialStateProperty.all(
+//       const Color(0xFF212529),
+//     ),
+//     textStyle: MaterialStateProperty.all(
+//       const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+//     ),
+//   );
+// }
+
+abstract class AppButtonStyle2 {
+  static final ButtonStyle linkButton2 = ButtonStyle(
+    foregroundColor: MaterialStateProperty.all(
+      const Color(0xFF01B4E4),
+    ),
+    textStyle: MaterialStateProperty.all(
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+    ),
+  );
+}
--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kino/Theme/app_button_style.dart';
 
 class AuthWidget extends StatefulWidget {
   const AuthWidget({Key? key}) : super(key: key);
@@ -33,19 +34,148 @@ class _HeaderWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(height: 25),
+          _FormWidget(),
+          SizedBox(height: 25),
           Text(
-            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. Click here  to get started.',
+            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
             style: textStyle,
+          ),
+          SizedBox(height: 5),
+          TextButton(
+            style: AppButtonStyle2.linkButton2,
+            onPressed: () {},
+            child: Text('Register'),
           ),
           SizedBox(height: 25),
           Text(
-            'If you signed up but didn`t get your verification email, click here to have it resent.',
+            'If you signed up but didn`t get your verification email.',
             style: textStyle,
+          ),
+          SizedBox(height: 5),
+          TextButton(
+            style: AppButtonStyle2.linkButton2,
+            onPressed: () {},
+            child: Text('Verify email'),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  _FormWidget({Key? key}) : super(key: key);
+
+  @override
+  State<_FormWidget> createState() => __FormWidgetState();
+}
+
+class __FormWidgetState extends State<_FormWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  String? errorText = null;
+
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+      // print('open app');
+    } else {
+      errorText = 'Неверный пароль или логин';
+      // print('show error');
+    }
+    setState(() {});
+  }
+
+  void _resetPassword() {
+    print('reset password');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = const Color(0xFF01B4E4);
+    final textStyle = const TextStyle(
+      fontSize: 16,
+      color: Color(0xFF212529),
+    );
+    final textFieldDecorator = const InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      isCollapsed: true,
+    );
+    final errorText = this.errorText;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.red,
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+        Text(
+          'Username',
+          style: textStyle,
+        ),
+        SizedBox(height: 5),
+        TextField(
+          controller: _loginTextController,
+          decoration: textFieldDecorator,
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Password',
+          style: textStyle,
+        ),
+        SizedBox(height: 5),
+        TextField(
+          controller: _passwordTextController,
+          decoration: textFieldDecorator,
+          obscureText: true,
+        ),
+        SizedBox(height: 25),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: _auth,
+              child: Text('Login'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(color),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                ),
+              ),
+            ),
+            SizedBox(width: 25),
+            TextButton(
+              onPressed: _resetPassword,
+              child: Text('Reset password'),
+              style: AppButtonStyle2.linkButton2,
+
+              // ButtonStyle(
+              //   // foregroundColor: MaterialStateProperty.all(
+              //   //   const Color(0xFF212529),
+              //   // ),
+              //   // textStyle: MaterialStateProperty.all(
+              //   //   const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+              //   // ),
+              // ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
