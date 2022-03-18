668d93e70bf561708a7b1ce98bc84d50af9c7d58
zavorgan <zavorgan@gmail.com>
2022-03-18 17:52:22 +0900

    18/03/2022_17:52

--- lib/widgets/main_screen/main_screen_widget.dart
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class MianScreenWidget extends StatefulWidget {
@@ -8,11 +10,53 @@ class MianScreenWidget extends StatefulWidget {
 }
 
 class _MianScreenWidgetState extends State<MianScreenWidget> {
+  int _selectedTab = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Новости',
+    ),
+    Text(
+      'Фильмы',
+    ),
+    Text(
+      'Сериалы',
+    ),
+  ];
+
+  void onSelectTab(int index) {
+    if (_selectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('TMDB'),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedTab],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        // ignore: prefer_const_literals_to_create_immutables
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Новости',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_filter),
+            label: 'Фильмы',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'Сериалы',
+          ),
+        ],
+        onTap: onSelectTab,
       ),
     );
   }
